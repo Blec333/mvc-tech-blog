a978ec85b018d7e234eab5bf63ae57b963605ace
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-08 22:42:47 -0700

    add signup fetch post

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+
+const signupFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector("#user_signup").value.trim();
+    const password = document.querySelector("#pass_signup").value.trim();
+    
+    const response = await fetch('/api/users/', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up');
+    }
+  };
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
