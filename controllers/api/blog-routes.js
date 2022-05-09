b253861255ac1fbdea29e7209bff559e90565249
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-08 22:17:54 -0700

    add api blog routes

---
+++ controllers/api/blog-routes.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const { Blog } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const getBlog = await Blog.findAll(req.body);
+    res.json(getBlog);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+  const getBlog = await Blog.findByPk(req.params.id, {
+    // include: [{ model: Comment }],
+  });
+  if (!getBlog) {
+    res.status(404).json({ message: 'No blog found with that id!' });
+    return;
+  }
+  res.status(200).json(getBlog);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      title: req.body.title,
+      contents: req.body.contents,
+      userId: req.session.userId,
+    });
+    res.json(newBlog);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const updateBlog = await Blog.update(req.body);
+    res.json(updateBlog);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const deleteBlog = await Blog.destroy(req.body);
+    res.json(deleteBlog);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+module.exports = router;
