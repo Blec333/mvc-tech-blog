21af11e1104bf5992a635d7d72bb6b9b1ec8daf0
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-08 22:16:56 -0700

    add home routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const { Blog, User } = require("../models/");
+
+router.get("/", (req, res) => {
+  console.log(req.session);
+    res.render("home");
+});
+
+router.get("/blogs", async (req, res) => {
+  try {
+    const blogs = (await Blog.findAll({ include: [User],})).map((blog) =>
+      blog.get({ plain: true })
+    );
+     res.render("blogs", { blogs });
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+router.get("/blogs/:id", async (req, res) => {
+  try {
+    const blog = (await Blog.findByPk(req.params.id, { include: [User],})).get({ plain: true });
+    res.render("singleblog", { ...blog });
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+module.exports = router;
