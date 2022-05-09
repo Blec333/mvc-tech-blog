6bff2d88da36199fb1a4424fdb438e1a9b6d0f27
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-08 22:41:46 -0700

    add new blog fetch post

---
+++ public/js/new-blog.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+    const title = document.querySelector("#blog_title").value;
+    const contents = document.querySelector("#blog_contents").value;
+    
+    const response = await fetch(`/api/blogs`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        contents,
+        
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    
+    if (response.ok) {
+      document.location.replace("/dashboard/blogs");
+    } else {
+      alert("Failed to add a new blog");
+    }
+  }
+  
+  document.querySelector(".new-blog-form").addEventListener("submit", newFormHandler);
