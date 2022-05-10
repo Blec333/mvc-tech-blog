5bea13560c17554f195b839e108c07cf2e2e741c
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-09 21:51:05 -0700

    turn on jaws db connection

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 const connection =
-  // process.env.JAWSDB_URL
-  false
+  process.env.JAWSDB_URL
+  // false
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: "localhost",
